304b178d93941c234b0046f6f1520ce6a8609a23
Daria Pakshina <daradasa7@yandex.ru>
2024-10-14 09:47:42 +0300

    initial-sommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [NgFor],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   suggestUserName() {
